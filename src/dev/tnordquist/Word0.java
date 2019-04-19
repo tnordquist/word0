ff7161cfea557ab8ea670f8170dd50b1b86bb01f
Todd A Nordquist <toddnordquist144@gmail.com>
2019-04-19 08:02:12 -0600

    Initial commit, solution to codingbat problem Word0.

---
+++ src/dev/tnordquist/Word0.java
@@ -0,0 +1,27 @@
+package dev.tnordquist;
+
+import java.util.HashMap;
+import java.util.Map;
+
+/**
+ * Given an array of strings, return a Map<String, Integer> containing a key for every different
+ * string in the array, always with the value 0. For example the string "hello" makes the pair
+ * "hello":0. We'll do more complicated counting later, but for this problem the value is simply 0.
+ */
+public class Word0 {
+
+  public Map<String, Integer> word0(String[] strings) {
+
+    Map<String, Integer> map = new HashMap<>();
+
+    for (int i = 0; i < strings.length; ++i) {
+      if (!map.containsKey(strings[i])) {
+        map.put(strings[i], 0);
+      }
+    }
+
+    return map;
+
+  }
+
+}
